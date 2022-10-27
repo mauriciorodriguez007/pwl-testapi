e4faf25e8ffc8a63415ad744d6aace827b12ea0b
Mauricio Rodriguez <Mauricio.Rodriguez@redsailtechnologies.com>
2022-10-27 00:45:33 -0400

    more changes.

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
     "fmt"
     "net/http"
+	"time"
 )
 
 func main() {
+	addr := ":7777"
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Printf("\n%sPWL-testapi new request from %v body: %v",time.Now(),r.RemoteAddr,r.Body)
+        fmt.Fprintf(w, "%s HELLO WORLD, you've requested: %s\n",time.Now(),r.URL.Path)
     })
-
-    http.ListenAndServe(":80", nil)
-	fmt.Printf("testing new change")
-	// adding another line 
-	// adding line # 2 
+	fmt.Printf("\n%sPWL-testapi listening on %v",time.Now(),addr)
+    err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Printf("\n%s listenandserve failed error: %s",time.Now(),err)	
+	}	
 }
