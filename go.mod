946f02fe1f123bfb95eb9566e40fddfe7f01e333
Mauricio Rodriguez <Mauricio.Rodriguez@redsailtechnologies.com>
2022-11-05 14:24:06 -0400

    added pipe

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module pwl-testapi
 
 go 1.19
+
+require (
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,90 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net"
+	"os"
 	"time"
+
+	"github.com/Microsoft/go-winio"
 )
 
 func main() {
-	addr := ":7777"
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("\n%sPWL-testapi new request from %v body: %v",time.Now(),r.RemoteAddr,r.Body)
-        fmt.Fprintf(w, "%s HELLO WORLD, you've requested: %s\n",time.Now(),r.URL.Path)
-    })
-	fmt.Printf("\n%sPWL-testapi listening on %v",time.Now(),addr)
-    err := http.ListenAndServe(addr, nil)
+	pipePath := `\\.\pipe\mypipename`
+
+	if err := os.RemoveAll(pipePath); err != nil {
+		log.Fatal(err)
+	}
+
+	pc := &winio.PipeConfig{
+		SecurityDescriptor: "D:P(A;;GA;;;AU)",
+		InputBufferSize:    512,
+		OutputBufferSize:   512,
+		MessageMode: true,
+	}
+
+	l, err := winio.ListenPipe(pipePath, pc)
+
 	if err != nil {
-		fmt.Printf("\n%s listenandserve failed error: %s",time.Now(),err)	
-	}	
+		log.Fatal("listen error:", err)
+	}
+	defer l.Close()
+
+	go WritePipe("My message")
+	for {
+		fmt.Printf("waiting on message")
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("accept error:", err)
+		}
+		fmt.Println("got a connection - dispatching to handler")
+		go printMsgServer(conn)
+	}
+	//ConnectPipe()
+
+	wait := make(chan string)
+	<-wait
+	//fmt.Printf("exiting main function")
+}
+
+func printMsgServer(c net.Conn) {
+	fmt.Println("in the handler")
+	log.Printf("Client connected [%s]", c.RemoteAddr().Network())
+
+	buf := make([]byte, 512)
+	for true {
+		fmt.Printf("read blocking...")
+		n, err := c.Read(buf)
+		fmt.Printf("read %d bytes\n", n)
+		if err != nil {
+			str := string(buf)
+			fmt.Printf("got message: %s\n", str)
+		}
+	}
+
+}
+func ConnectPipe() {
+
+}
+
+func ReadPipe() {
+
+}
+
+func WritePipe(msg string) {
+	fmt.Printf("waiting 10 seconds")
+	time.Sleep(10 * time.Second)
+	pipePath := `\\.\pipe\mypipename`
+	f, err := os.OpenFile(pipePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	for true {
+		fmt.Printf("writing to pipe")
+		f.WriteString("message from client\n")		
+		time.Sleep(1 * time.Second)		
+	}
+	f.Close()
+	fmt.Println("done writing to pipe")
 }
